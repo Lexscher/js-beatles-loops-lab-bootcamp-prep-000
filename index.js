0c13c1926f7c8db88bcb3a303277314a8540f4d8
Alexander Schelchere <ascellie@gmail.com>
2019-04-25 15:09:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// add solution here
+function theBeatlesPlay(museArr, insArr) {
+  let arr = [];
+  for (let i in museArr) {
+    arr.push(`${museArr[i]} plays ${insArr[i]}`)
+  }
+  return arr;
+}
+
+function iLoveTheBeatles(num) {
+  let luv = [];
+  do {
+    
+  } while ();
+  return;
+}
